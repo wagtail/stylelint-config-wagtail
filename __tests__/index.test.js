9c85870cdd249a7af6cdee156ca8c03ce82fe60a
LB Johnston <lb.johnston@virginaustralia.com>
2019-04-25 16:11:14 +1000

    Add Jest dependency and initial basic tests
     - Tests will validate the stylelint config works as expected

---
+++ __tests__/index.test.js
@@ -0,0 +1,36 @@
+const config = require('../');
+const fs = require('fs');
+const stylelint = require('stylelint');
+
+const validScss = fs.readFileSync('./__tests__/scss-valid.scss', 'utf-8');
+const invalidScss = fs.readFileSync('./__tests__/scss-invalid.scss', 'utf-8');
+
+describe('flags no warnings with valid css', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: validScss,
+      config
+    });
+  });
+
+  it('did not error', () =>
+    result.then(data => expect(data.errored).toBeFalsy()));
+
+  it('flags no warnings', () =>
+    result.then(data => expect(data.results[0].warnings).toHaveLength(0)));
+});
+
+describe('flags warnings with invalid css', () => {
+  let result;
+
+  beforeEach(() => {
+    result = stylelint.lint({
+      code: invalidScss,
+      config
+    });
+  });
+
+  it('did error', () => result.then(data => expect(data.errored).toBeTruthy()));
+});
